9316c72a006905b4ffc0f9ed7c53e000984612ab
Arun Prakash <arun@entrayn.com>
2015-08-18 16:23:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,12 +10,47 @@ $("#scrollquestion").animate({scrollLeft: leftPos - 200}, 800);
 $("#rightArrow").click(function () {
   var leftPos = $('#scrollquestion').scrollLeft();
 $("#scrollquestion").animate({scrollLeft: leftPos + 200}, 800);
+
+
+
 });
+
+$('#scrollquestion').dragscrollable();
 
 $(".c").click(function(){
    $(this).addClass("question-viewed");
    $(this).addClass("current-question");
    $(this).siblings().removeClass("current-question");
+});
+
+          $('#rightArrow').click(function(){
+          $('#leftArrow').removeClass("leftArrowDisable");
+        });
+
+
+$(function() {
+    $('#scrollquestion').scroll( function() {
+        var $width = $('#scrollquestion').outerWidth()
+        var $scrollWidth = $('#scrollquestion')[0].scrollWidth;
+        var $scrollLeft = $('#scrollquestion').scrollLeft();
+
+        if ($scrollWidth - $width=== $scrollLeft){
+            $('#rightArrow').addClass("rightArrowDisable");
+            $('#leftArrow').removeClass("leftArrowDisable");
+        }
+        if ($scrollLeft===0){
+             $('#leftArrow').addClass("leftArrowDisable");
+            $('#rightArrow').removeClass("rightArrowDisable");
+        }
+        $('#leftArrow').click(function(){
+          $('#rightArrow').removeClass("rightArrowDisable");
+        });
+
+          $('#rightArrow').click(function(){
+          $('#leftArrow').removeClass("leftArrowDisable");
+        });
+
+    });
 });
 
 
@@ -27,6 +62,7 @@ function fillQuestionNumber(questionNum){
     $("#scrollquestion").append('<div class="c"><h6>'+i+'</h6></div>');
   }
 }
+
 
 
 
